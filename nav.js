97ac1c2323f9fc0901673d856d4c823029498bb2
Mete <mete.yalcin.05@gmail.com>
2023-12-31 20:56:56 +0100

    fixed animations and unclosed elements

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
 document.write(`<header class="barNavigation">
     <a href="/index.html" class="boutonNavigation">Accueil</a>
     <a href="/html/CV.html  " class="boutonNavigation">CV</a>
-    <a href="/html/interets.html" class="boutonNavigation">Centres d'interêts</a>
+    <a href="/html/interets.html" class="boutonNavigation">Centres d'intérêt</a>
     <a href="/html/projets.html" class="boutonNavigation">Projets</a>
     <a href="/html/contact.html" class="boutonNavigation">Contact</a>
     </header>`);
