bc37cd3d621a4f006d05df3a05365c48802351cb
Mete <mete.yalcin.05@gmail.com>
2023-12-31 21:31:19 +0100

    class rename

--- nav.js
+++ nav.js
@@ -1,7 +1,7 @@
-document.write(`<header class="barNavigation">
-    <a href="/index.html" class="boutonNavigation">Accueil</a>
-    <a href="/html/CV.html  " class="boutonNavigation">CV</a>
-    <a href="/html/interets.html" class="boutonNavigation">Centres d'intérêt</a>
-    <a href="/html/projets.html" class="boutonNavigation">Projets</a>
-    <a href="/html/contact.html" class="boutonNavigation">Contact</a>
+document.write(`<header class="navigation-bar">
+    <a href="/index.html" class="navigation-button">Accueil</a>
+    <a href="/html/CV.html  " class="navigation-button">CV</a>
+    <a href="/html/interets.html" class="navigation-button">Centres d'intérêt</a>
+    <a href="/html/projets.html" class="navigation-button">Projets</a>
+    <a href="/html/contact.html" class="navigation-button">Contact</a>
     </header>`);
